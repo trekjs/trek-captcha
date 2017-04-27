03aa67b665f2a38c519033f429a7316c2f92e4c4
shilong.sang <i5ting@126.com>
2017-04-27 11:29:47 +0800

    refact, please ignore

--- demo.js
+++ demo.js
@@ -2,4 +2,7 @@ var captcha = require('.')
 
 captcha().then(function(obj) {
     console.log(obj.token)
+
+    const fs = require('fs')
+    fs.createWriteStream('a.gif').on('finish', () => console.log(obj.token)).end(obj.buffer)
 })
--- test/index.js
+++ test/index.js
@@ -7,3 +7,11 @@ test('should return an object and include token and buffer', function(t){
     t.is(obj.buffer.length, 17646)
   })
 })
+
+
+test('should return an object and include token and buffer', function(t){
+  return captcha(6).then(function(obj) {
+    t.is(obj.token.length, 6)
+    t.is(obj.buffer.length, 17646)
+  })
+})
